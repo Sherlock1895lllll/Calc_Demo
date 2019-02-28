dd5360099ed383b80e7a17b1ca338089c897a171
Sherlock1895lllll <434710437@qq.com>
2019-02-28 13:23:32 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("用户从控制台输入的数字为："+args[0]);
+		System.out.println("The number entered by the user from the console is:"+args[0]);
 		String path = "result.txt";
 		WriteToFile wtf = new WriteToFile(path);
 		System.out.println("Success!");
